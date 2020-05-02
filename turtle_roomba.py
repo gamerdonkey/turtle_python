19e056add059bdd0c9c806475e2080c5cf2a1d79
gamerdonkey <gamerdonkey@gmail.com>
2020-05-02 17:42:28 -0500

    Changed the way my 'roomba' simlulator works so that it's more like how I want. It calculates its path before moving instead of doing a move-and-check to stay within boundaries.

--- turtle_roomba.py
+++ turtle_roomba.py
@@ -3,37 +3,60 @@ import random
 
 from turtle import *
 
-#speed(10)
+speed(10)
 
 BOX_SIZE = 300
 
 bgcolor("black")
 pencolor("white")
 
-for i in range(4):
-   forward(BOX_SIZE)
-   left(90)
+#for i in range(4):
+#   forward(BOX_SIZE)
+#   left(90)
 
 penup()
-setposition(0, random.randint(0, BOX_SIZE))
+setposition(random.randint(0, BOX_SIZE), BOX_SIZE)
 pendown()
 
-while(True):
-   pencolor(max(0, min(xcor() / BOX_SIZE, 1)), max(0, min(ycor() / BOX_SIZE, 1)), 0.1)
+def forward_to_boundary(max_adj, max_opp, heading_rads):
+   if(abs(max_adj * math.tan(heading_rads)) < abs(max_opp)):
+      forward(max_adj / math.cos(heading_rads))
+   else:
+      forward(max_opp / math.sin(heading_rads))
 
-   if((not 0 < xcor() < BOX_SIZE) or (not 0 < ycor() < BOX_SIZE)):
-      if(0 < xcor() < BOX_SIZE):
-         if(ycor() >= BOX_SIZE):
-            setheading(random.randint(180, 360))
-         else:
-            setheading(random.randint(0, 180))
+
+while(True):
+   # pencolor(max(0, min(xcor() / BOX_SIZE, 1)), max(0, min(ycor() / BOX_SIZE, 1)), 0.1)
+   pencolor(random.random(), random.random(), random.random())
+
+   if(0 < round(xcor()) < BOX_SIZE):
+      if(round(ycor()) >= BOX_SIZE):
+         setheading(random.randint(180, 360))
+         opposite = -ycor()
       else:
-         if(xcor() >= BOX_SIZE):
-            setheading(random.randint(90, 270))
-         else:
-            setheading(random.randint(0, 180) - 90)
-   #print("Position: ", position(), " Heading: ", heading())            
-   forward(2)
+         setheading(random.randint(0, 180))
+         opposite = BOX_SIZE
+
+
+      if(math.cos(math.radians(heading())) > 0):
+         adjacent = BOX_SIZE - xcor()
+      else:
+         adjacent = -xcor()
+
+   else:
+      if(round(xcor()) >= BOX_SIZE):
+         setheading(random.randint(90, 270))
+         adjacent = -xcor()
+      else:
+         setheading(random.randint(0, 180) - 90)
+         adjacent = BOX_SIZE
+
+      if(math.sin(math.radians(heading())) > 0):
+         opposite = BOX_SIZE - ycor()
+      else:
+         opposite = -ycor()
+   
+   forward_to_boundary(adjacent, opposite, math.radians(heading()))
 
 print("Final position: ", position())
 print("Absolute position: ", abs(position()))
